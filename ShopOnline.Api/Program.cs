94ddba1e7a80d62550e458bbdf06ea6f83f175fb
Guilherme Pirani <45105007+guilhermepirani@users.noreply.github.com>
2023-02-20 12:09:46 -0300

    all products page

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Net.Http.Headers;
 using ShopOnline.Api.Data;
 using ShopOnline.Api.Repositories;
 using ShopOnline.Api.Repositories.Contracts;
@@ -33,6 +34,12 @@ namespace ShopOnline.Api
                 app.UseSwagger();
                 app.UseSwaggerUI();
             }
+
+            app.UseCors(policy =>
+                policy.WithOrigins("http://localhost:7289", "https://localhost:7289")
+                .AllowAnyMethod()
+                .WithHeaders(HeaderNames.ContentType)
+            ); 
 
             app.UseHttpsRedirection();
 
